d9603e1f3110a76aba5cc03608e9717635f602e4
David Golden <dagolden@cpan.org>
2013-09-23 17:22:40 +0000

    Import of DAGOLDEN/Parse-CPAN-Meta-1.4409 from CPAN.
    gitpan-cpan-distribution: Parse-CPAN-Meta
    gitpan-cpan-version:      1.4409
    gitpan-cpan-path:         DAGOLDEN/Parse-CPAN-Meta-1.4409.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Parse/CPAN/Meta.pm
+++ lib/Parse/CPAN/Meta.pm
@@ -1,7 +1,7 @@
 use strict;
 package Parse::CPAN::Meta;
 # ABSTRACT: Parse META.yml and META.json CPAN metadata files
-our $VERSION = '1.4408'; # VERSION
+our $VERSION = '1.4409'; # VERSION
 
 use Carp 'croak';
 
@@ -136,7 +136,7 @@ Parse::CPAN::Meta - Parse META.yml and META.json CPAN metadata files
 
 =head1 VERSION
 
-version 1.4408
+version 1.4409
 
 =head1 SYNOPSIS
 
